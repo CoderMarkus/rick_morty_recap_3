239beecff0f7f91ede1b66cd3c0f2e913faf1ce8
Rabe Christian <rabenklar@gmx.de>
2023-03-06 15:53:39 +0100

    backup index.js

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
+import searchQuery from "./components/search-bar/search-bar.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
-const searchBar = document.querySelector('[data-js="search-bar"]');
+export const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
+export const input = document.querySelector('[data-js="search-bar__input"]');
 
 // States
 const maxPage = 1;
 const page = 1;
-const searchQuery = "";
+
+// async function fetchCharacters() {
+
+//   const response = await fetch(
+//     "https://rickandmortyapi.com/api/character?page=<pageIndex>"
+//   );
+//   const data = await response.json();
+//   return data;
+// }
+
+// prevButton = createNavButton("prev", () => {
+//   if (page <= 1) return;
+//   page--;
+//   fetchCharacters();
+// });
+
+// nextButton = createNavButton("next", () => {
+//   if (page >= 1) return;
+//   page++;
+//   fetchCharacters();
+// });
+
+// pagination = createPagination();
+
+// navigation.append(prevButton, pagination, nextButton);
+pagination.textContent = `${page} / ${maxPage}`;
+
+
+
+
+
+// const searchQuery = () =>  {
+// searchBar.addEventListener("submit", (event) => {
+// event.preventDefault();
+// const inputvalue = input.value;
+// console.log('sq', inputvalue)
+// return inputvalue;
+// });
+// }
+searchQuery();
